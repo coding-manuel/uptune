510c5f8c5f3931442159ed1ce1dd1c27e084507a
Gautam Manuel <gautam.manuel@gmail.com>
2022-05-28 19:58:14 +0530

    Changes to comment drawer

--- src/components/CommentDrawer.jsx
+++ src/components/CommentDrawer.jsx
@@ -1,24 +1,27 @@
 import React,{useEffect, useContext, useState} from 'react'
 import { MusicContext } from '../context/MusicContext';
-import { Drawer, Group, Text, Stack, Box, Paper, Button } from '@mantine/core';
+import { Drawer, Group, Text, Stack, Box, Paper, Button, useMantineTheme } from '@mantine/core';
 import { CurrencyEth } from 'phosphor-react';
 import { UptuneContext } from '../context/UptuneContext';
 import ReactTimeAgo from 'react-time-ago'
 
 const Comment = ({commentData}) => {
+    const theme = useMantineTheme()
     return (
-    <Group sx={{justifyContent: 'space-between'}} my={12}>
-        <Box sx={{backgroundColor: '#353535', padding: 16, borderRadius: 4, width: 120}}>
-            <Group sx={{justifyContent: 'space-between'}}>
-                <Text size='md' weight={600}>{commentData.amount}</Text>
-                <CurrencyEth size={14} />
-            </Group>
-        </Box>
-        <Stack align='flex-end' sx={{gap: 2}}>
-            <Text weight={600}>{commentData.comment}</Text>
-            <Text size='xs' weight={600}><ReactTimeAgo date={commentData.timestamp} locale="en-US"/></Text>
-        </Stack>
-    </Group>
+    <Paper shadow="sm" p="md" my={12}>
+        <Group sx={{justifyContent: 'space-between'}} >
+            <Box sx={{border: `1px solid ${theme.colors.gray[8]}`, padding: 16, borderRadius: 4, width: 120}}>
+                <Group sx={{justifyContent: 'space-between'}}>
+                    <Text size='md' weight={600}>{commentData.amount}</Text>
+                    <CurrencyEth size={14} />
+                </Group>
+            </Box>
+            <Stack align='flex-end' sx={{gap: 2}}>
+                <Text weight={600}>{commentData.comment}</Text>
+                <Text size='xs' weight={600}><ReactTimeAgo date={commentData.timestamp} locale="en-US"/></Text>
+            </Stack>
+        </Group>
+    </Paper>
 )}
 
 export default function CommentDrawer({songData}) {
