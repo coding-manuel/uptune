88a664ed1a34136ac639ec6827ecb6838bee098f
Gautam Manuel <gautam.manuel@gmail.com>
2022-05-29 10:08:41 +0530

    Fixed Song Fetching

--- src/context/MusicContext.jsx
+++ src/context/MusicContext.jsx
@@ -3,11 +3,14 @@ import { UptuneContext } from "./UptuneContext";
 
 export const MusicContext = React.createContext()
 
+const genresList = ['Rock', 'Jazz', 'Dubstep', 'Techno', 'Pop', 'Classical']
+
 export const MusicProvider = ({children}) => {
     const {getAllAudio} = useContext(UptuneContext);
     const [AllSongs, setAllSongs] = useState([]);
 
     const [mostTipped, setMostTipped] = useState([]);
+    const [genreList, setGenreList] = useState({})
 
     const [songData, setSongData] = useState([]);
     const [modalOpen, setModalOpen] = useState(false);
@@ -20,14 +23,27 @@ export const MusicProvider = ({children}) => {
 
     const createFilter = async () => {
         let mostTippedSongs = []
+        let genreListSongs = {}
 
         if(AllSongs.length !== 0){
             AllSongs.map((song) => {
                 if(parseInt(song.amount) > 3){
                     mostTippedSongs.push(song)
                 }
+
+                for(let i = 0; i < song.genres.length; i++){
+                    let x = genreListSongs[song.genres[i].toLowerCase()]
+                    if(x)
+                        genreListSongs[song.genres[i].toLowerCase()].push(song)
+                    else
+                        genreListSongs[song.genres[i].toLowerCase()] = [song]
+                }
+
             })
             setMostTipped(mostTippedSongs)
+            setGenreList(genreListSongs)
+
+            console.log(genreListSongs)
         }
     }
 
@@ -40,7 +56,7 @@ export const MusicProvider = ({children}) => {
     }, [])
 
     return(
-        <MusicContext.Provider value={{songData, setSongData, modalOpen, setModalOpen, commentDrawer, setCommentDrawer, AllSongs, mostTipped, fetchSongs}}>
+        <MusicContext.Provider value={{songData, setSongData, modalOpen, setModalOpen, commentDrawer, setCommentDrawer, AllSongs, mostTipped, fetchSongs, genreList}}>
             {children}
         </MusicContext.Provider>
     )
--- src/pages/Genre.jsx
+++ src/pages/Genre.jsx
@@ -1,12 +1,32 @@
-import { Title, useMantineTheme } from '@mantine/core'
-import React from 'react'
+import React, {useContext, useEffect, useState} from 'react'
+import { Title, Group, useMantineTheme } from '@mantine/core'
 import { useParams } from 'react-router-dom'
+import { MusicContext } from '../context/MusicContext'
+import SongThumb from '../components/SongThumb';
 
 export default function Genre() {
+    const {genreList} = useContext(MusicContext);
+
+    const [localGenreList, setLocalGenreList] = useState({});
     const {genre} = useParams()
     const theme = useMantineTheme()
 
+    useEffect(() => {
+        console.log(genreList)
+        setLocalGenreList(genreList)
+    }, [genreList])
+
     return (
-        <Title order={5}><span style={{color: theme.colors.red[7]}}>{genre.charAt(0).toUpperCase() + genre.slice(1)}</span> Songs</Title>
+        <>
+            <Title order={5}><span style={{color: theme.colors.red[7]}}>{genre.charAt(0).toUpperCase() + genre.slice(1)}</span> Songs</Title>
+
+            <Group my={12} position='center' grow>
+                {Object.keys(localGenreList).length !== 0 && localGenreList[genre].map((song) => {
+                    return(
+                        <SongThumb song={song} />
+                    )
+                })}
+            </Group>
+        </>
     )
 }
